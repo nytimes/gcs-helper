5dc779d77b792c29ee7818b2d735eda0f8462f3a
Francisco Souza <francisco.souza@nytimes.com>
2017-06-01 17:55:17 -0400

    Use logrus logger on main

--- main.go
+++ main.go
@@ -14,22 +14,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	logger := config.logger()
 	client, err := storage.NewClient(context.Background())
 	if err != nil {
-		log.Fatal(err)
+		logger.WithError(err).Fatal("failed to create storage client instance")
 	}
 	handler, err := getHandler(config, client)
 	if err != nil {
-		log.Fatal(err)
+		logger.WithError(err).Fatal("failed to get handle instance")
 	}
 	listener, err := net.Listen("tcp", config.Listen)
 	if err != nil {
-		log.Fatal(err)
+		logger.WithField("listenAddr", config.Listen).WithError(err).Fatal("failed to start listener")
 	}
 
-	log.Printf("Listening on %s...", listener.Addr())
+	logger.Infof("Listening on %s...", listener.Addr())
 	err = http.Serve(listener, handler)
 	if err != nil {
-		log.Fatal(err)
+		logger.WithError(err).Fatal("failed to start server")
 	}
 }
