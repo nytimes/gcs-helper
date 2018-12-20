4969eba531125d7f734bf01dd89974b96230403c
Francisco Souza <francisco.souza@nytimes.com>
2018-12-19 22:17:01 -0500

    internal/regexpcache: add tests

---
+++ internal/regexpcache/cache_test.go
@@ -0,0 +1,42 @@
+package regexpcache
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMustCompile(t *testing.T) {
+	t.Parallel()
+
+	re := MustCompile("^$")
+	cached := MustCompile("^$")
+	if re != cached {
+		t.Errorf("didn't cache regexp\nwant %#v\ngot  %#v", re, cached)
+	}
+
+	other := MustCompile("^ $")
+	if other == re {
+		t.Errorf("poluted cache?")
+	}
+}
+
+func TestMustCompilePanicsOnInvalidRegexp(t *testing.T) {
+	t.Parallel()
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("didn't panic on invalid regexp")
+		}
+	}()
+
+	MustCompile("[0----]")
+}
+
+func TestMain(m *testing.M) {
+	cache.Range(func(k, _ interface{}) bool {
+		cache.Delete(k)
+		return true
+	})
+	os.Exit(m.Run())
+}
