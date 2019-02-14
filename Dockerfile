987edad7e0fafe183c752681ada70b5b39745c5d
Francisco Souza <francisco.souza@nytimes.com>
2019-02-13 22:11:34 -0500

    Build Docker image on travis, after running all steps

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,4 @@
-FROM    golang:1.12rc1-alpine AS build
-ENV     CGO_ENABLED 0
-RUN     apk add --no-cache git
-ADD     . /code
-WORKDIR /code
-RUN     go install
-
 FROM alpine:3.9
 RUN  apk add --no-cache ca-certificates
-COPY --from=build /go/bin/gcs-helper /usr/bin/gcs-helper
+ADD  gcs-helper /usr/bin/gcs-helper
 ENTRYPOINT ["/usr/bin/gcs-helper"]
---
+++ travis-scripts/docker-build.bash
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+	echo >&2 "Skipping image build on pull requests..."
+	exit 0
+fi
+
+if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
+	echo >&2 "Skipping image build on cron..."
+	exit 0
+fi
+
+if [ "${TRAVIS_GO_VERSION}" != "1.x" ]; then
+	echo >&2 "Skipping docker build on Go ${TRAVIS_GO_VERSION}"
+	exit 0
+fi
+
+if [ "${TRAVIS_BRANCH}" != "master" ] && [ -z "${TRAVIS_TAG}" ]; then
+	echo >&2 "Skipping docker build on branch ${TRAVIS_BRANCH}"
+	exit 0
+fi
+
+IMAGE_NAME=nytimes/gcs-helper
+
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker build -t ${IMAGE_NAME}:latest -f Dockerfile.ci .
+
+if [ -n "${TRAVIS_TAG}" ]; then
+	docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TRAVIS_TAG}
+	docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TRAVIS_TAG%%.*}
+fi
+
+docker push ${IMAGE_NAME}
