f961d35cf92cf1223ec187a2e2c7d18bfb61eb6e
Francisco Souza <francisco.souza@nytimes.com>
2019-02-26 17:55:38 -0500

    travis-scripts: build the docker image on all pushes and PRs
    Skip only the push part, but still validate that we can build the image
    on pushes/PRs.

--- travis-scripts/docker-build.bash
+++ travis-scripts/docker-build.bash
@@ -1,27 +1,27 @@
 #!/bin/bash -e
 
-export CGO_ENABLED=0
-
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-	echo >&2 "Skipping image build on pull requests..."
-	exit 0
-fi
-
 if [ "${TRAVIS_GO_VERSION}" != "1.x" ]; then
 	echo >&2 "Skipping docker build on Go ${TRAVIS_GO_VERSION}"
 	exit 0
 fi
 
-if [ "${TRAVIS_BRANCH}" != "master" ] && [ -z "${TRAVIS_TAG}" ]; then
-	echo >&2 "Skipping docker build on branch ${TRAVIS_BRANCH}"
+IMAGE_NAME=nytimes/gcs-helper
+
+export CGO_ENABLED=0
+go build -o gcs-helper
+docker build -t ${IMAGE_NAME}:latest .
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+	echo >&2 "Skipping docker push on pull requests..."
 	exit 0
 fi
 
-IMAGE_NAME=nytimes/gcs-helper
+if [ "${TRAVIS_BRANCH}" != "master" ] && [ -z "${TRAVIS_TAG}" ]; then
+	echo >&2 "Skipping docker push on branch ${TRAVIS_BRANCH}"
+	exit 0
+fi
 
-go build -o gcs-helper
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
-docker build -t ${IMAGE_NAME}:latest .
 
 if [ -n "${TRAVIS_TAG}" ]; then
 	docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TRAVIS_TAG}
